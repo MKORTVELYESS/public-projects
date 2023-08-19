b6b1b1fe010ba431e2238573d684ff173b4f7b3f
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2023-08-19 16:10:44 +0200

    Results container added to html. Script added that inserts the requred data into the html on the click event of the submit button. CSS styling enhanced to make cards the same size.

--- html css js/authors and their works/scripts.js
+++ html css js/authors and their works/scripts.js
@@ -1,8 +1,3 @@
-const inputSubmit = document.querySelector(
-  '.author-search input[type="submit"]'
-);
-const inputAuthor = document.getElementById("input-author-name");
-
 //Form submission needs to be prevented when submit button is clicked
 document.addEventListener("DOMContentLoaded", function () {
   const searchForm = document.getElementsByTagName("form")[0];
@@ -11,6 +6,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+//Add Evenet listener to submit button
+const inputSubmit = document.querySelector(
+  '.author-search input[type="submit"]'
+);
+inputSubmit.addEventListener("click", async function () {
+  const inputAuthor = document.getElementById("input-author-name");
+  const authorName = inputAuthor.value;
+  const authorsArr = await getAuthorsList(authorName);
+  authorsArr.forEach((author) => {
+    renderAuthorCard(author);
+  });
+});
+
+//Fetch API for author list matching function input
 async function getAuthorsList(authorName) {
   const url = `https://openlibrary.org/search/authors.json?q=${authorName}`;
   let response = await fetch(url);
@@ -18,8 +27,33 @@ async function getAuthorsList(authorName) {
   return response.docs;
 }
 
-inputSubmit.addEventListener("click", async function () {
-  const authorName = inputAuthor.value;
-  const authorsArr = await getAuthorsList(authorName);
-  renderAuthorCard(author);
-});
+//Render the author cards into the HTML with the required information and img
+async function renderAuthorCard(author) {
+  const resultsContainer = document.querySelector(
+    ".container.search-results.row"
+  );
+  resultsContainer.innerHTML = ""; //Remove the contents from previous query
+  const imgURL = `https://covers.openlibrary.org/a/olid/${author.key}-M.jpg`;
+  const response = await fetch(imgURL);
+  console.log(response);
+  const name = author.name;
+  let birthday = author.birth_date;
+  if (birthday === undefined) {
+    birthday = "Unknown birth date";
+  }
+
+  //Card is created with HTML due to the large number of classes to be added. Show works button calls function with inputs specified in HTML.
+  const cardHTML = `
+    <div class="col-lg-3 col-md-6 mb-4">
+    <div class="card m-auto">
+      <img class="card-img-top" src="${imgURL}" alt="Card image cap" />
+      <div class="card-body d-flex flex-column justify-content-between">
+        <div>
+          <h5 class="card-title mb-3">${name}</h5>
+          <h6 class="card-subtitle text-muted mb-1">${birthday}</h6>
+        </div>
+          <a href="#" class="btn btn-primary" onclick="showWorks('${author.key}','${name}')">Show works</a>
+      </div>
+      </div>`;
+  resultsContainer.insertAdjacentHTML("beforeend", cardHTML);
+}
