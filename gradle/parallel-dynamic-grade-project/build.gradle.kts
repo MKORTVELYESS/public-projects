3b60cf3ce3a51279a8bcd342f85eccf8541b6053
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2024-09-29 14:52:40 +0200

    Refactored command line property collection - each flagged task can now access falgs provider

--- gradle/parallel-dynamic-grade-project/build.gradle.kts
+++ gradle/parallel-dynamic-grade-project/build.gradle.kts
@@ -1,17 +1,18 @@
 //call to achieve dynamic flagging of commands like
     // ./gradlew sitePreparation -PsiteClearanceFlags="-d 2024-09-28" -PlandSurveyFlags="--date 2024.09.28" -PsitePreparationFlags="-D 20240928"
 
-tasks.register("sitePreparation") {//If paralellism work should take ~12 seconds
-    val flagsProvider: Provider<String> = project.providers.gradleProperty("sitePreparationFlags")
+abstract class FlaggedTask : DefaultTask() {
+    @Input // Shared property for all tasks to use, needs to marked as input https://docs.gradle.org/8.10/userguide/validation_problems.html#missing_annotation
+    val flagsProvider: Provider<String> = project.providers.gradleProperty("${name}Flags")
+}
+
+val sitePreparation by tasks.registering(FlaggedTask::class) {//If paralellism work should take ~12 seconds
     dependsOn(landSurvey, siteClearance) //site preparation depends on landSurvey and siteClearance which can happen paralelly to save time
     doLast {
         println("Site preparation in progress: Land surveying and site clearance for ${flagsProvider.getOrElse("")}")
-        // Add any additional logic here, such as simulating site preparation actions.
     }
 }
-val landSurvey by tasks.registering {//Takes 11 seconds
-    val flagsProvider: Provider<String> = project.providers.gradleProperty("landSurveyFlags")
-
+val landSurvey by tasks.registering(FlaggedTask::class) {//Takes 11 seconds
     doLast {
         println("Land survey started ${flagsProvider.getOrElse("")}")
         Thread.sleep(10000)
@@ -21,9 +22,7 @@ val landSurvey by tasks.registering {//Takes 11 seconds
     }
 }
 
-val siteClearance by tasks.registering { //Takes 11  seconds
-    val flagsProvider: Provider<String> = project.providers.gradleProperty("siteClearanceFlags")
-
+val siteClearance by tasks.registering(FlaggedTask::class) { //Takes 11  seconds
     doLast {
         println("Site clearance started ${flagsProvider.getOrElse("")}")
         Thread.sleep(10000)
