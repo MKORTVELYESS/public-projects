706c3b1c6744b5c1e3a4cd83e80fda9773f841d8
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2024-09-30 07:40:51 +0200

    Parse tasks from tasks.jsonm and execute them - temporarily disabled configuration cache

--- gradle/parallel-dynamic-grade-project/build.gradle.kts
+++ gradle/parallel-dynamic-grade-project/build.gradle.kts
@@ -1,37 +1,79 @@
 //call to achieve dynamic flagging of commands like
-// ./gradlew sitePreparation -PsiteClearanceFlags="-d 2024-09-28" -PlandSurveyFlags="--date 2024.09.28" -PsitePreparationFlags="-D 20240928"
-// Exclude some tasks which would normally be triggered by dependency: ./gradlew sitePreparation -x siteClearance -PsiteClearanceFlags="-d 2024-09-28" -PlandSurveyFlags="--date 2024.09.28" -PsitePreparationFlags="-D 20240928"
+// ./gradlew sitePreparation -PsitePreparationFlags="sitePreparationComplete" -PlandSurveyFlags="-n 10 127.0.0.1 >nul" -PsiteClearanceFlags="-n 12 127.0.0.1>nul"
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.util.*
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.0.20"
+}
+
+repositories {
+    mavenCentral()
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.fasterxml.jackson.core:jackson-databind:2.18.0")
+        classpath("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.0")
+        classpath("org.jetbrains.kotlin:kotlin-script-runtime:2.0.20")
+        classpath("org.jetbrains.kotlin:kotlin-scripting-jvm:2.0.20")
+        classpath("org.jetbrains.kotlin:kotlin-scripting-jvm-host:2.0.20")
+    }
+}
+
+fun executeShellCommand(command: String) {
+    val isWindows = System.getProperty("os.name").lowercase(Locale.getDefault()).contains("win")
+
+    val process = if (isWindows) {
+        ProcessBuilder("cmd.exe", "/c", command)
+    } else {
+        ProcessBuilder("sh", "-c", command)
+    }.redirectErrorStream(true).start()
+
+    val result = process.inputStream.bufferedReader().use { it.readText() }
+    println(result)
+}
+
+
+// Define a task to use jacksonObjectMapper within the task action
+
+val jsonFile = file("tasks.json")
+
+val mapper = jacksonObjectMapper()
+
+val list: TaskList = mapper.readValue(jsonFile)
+list.tasks.forEach { task ->
+    tasks.register(task.name, FlaggedTask::class) {
+        task.dependsOn.forEach { dependency ->
+            dependsOn(dependency)
+        }
+
+        doLast {
+            executeShellCommand("${task.action} ${flagsProvider.getOrElse("")}")
+        }
+    }
+}
 
 abstract class FlaggedTask : DefaultTask() {
     @Input // Shared property for all tasks to use, needs to marked as input https://docs.gradle.org/8.10/userguide/validation_problems.html#missing_annotation
     val flagsProvider: Provider<String> = project.providers.gradleProperty("${name}Flags")
 }
 
-val sitePreparation by tasks.registering(FlaggedTask::class) {//If paralellism work should take ~12 seconds
-    dependsOn(landSurvey, siteClearance) //site preparation depends on landSurvey and siteClearance which can happen paralelly to save time
-    doLast {
-        println("Site preparation in progress: Land surveying and site clearance for ${flagsProvider.getOrElse("")}")
-    }
-}
+data class Task(
+        val name: String,
+        val taskType: String,
+        val dependsOn: List<String>,
+        val action: String
+)
 
-val landSurvey by tasks.registering(FlaggedTask::class) {//Takes 11 seconds
-    doLast {
-        println("Land survey started ${flagsProvider.getOrElse("")}")
-        Thread.sleep(10000)
-        println("Land survey at 90%.")
-        Thread.sleep(1000)
-        println("Land survey complete")
-    }
-}
+data class TaskList(
+        val tasks: List<Task>
+)
 
-val siteClearance by tasks.registering(FlaggedTask::class) { //Takes 11  seconds
-    doLast {
-        println("Site clearance started ${flagsProvider.getOrElse("")}")
-        Thread.sleep(10000)
-        println("Site clearance at 90%.")
-        Thread.sleep(1000)
-        println("Site complete.")
-    }
-}
 
 
