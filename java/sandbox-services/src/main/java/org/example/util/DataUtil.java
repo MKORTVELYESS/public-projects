47f77d45d4b5519f8787cec3e64661a06c0cca08
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2025-12-06 16:41:30 +0100

    SAND-28 - [Test/Java] add data size evaluator

--- java/sandbox-services/src/main/java/org/example/util/DataUtil.java
+++ java/sandbox-services/src/main/java/org/example/util/DataUtil.java
@@ -6,24 +6,21 @@ import java.util.stream.Stream;
 public final class DataUtil {
 
   public static int countAllKeys(Object data) {
-    return count(data, new IdentityHashMap<>());
+    return count(data);
   }
 
-  private static int count(Object node, IdentityHashMap<Object, Boolean> visited) {
-
-    if (node == null || visited.containsKey(node)) return 0;
-
-    visited.put(node, true);
+  private static int count(Object node) {
+    if (node == null) return 0;
 
     return switch (node) {
-      case Map<?, ?> map -> map.size() + sumElements(map.values().stream(), visited);
-      case Collection<?> coll -> sumElements(coll.stream(), visited);
-      case Object[] arr -> sumElements(Arrays.stream(arr), visited);
-      default -> 0;
+      case Map<?, ?> map -> sumElements(map.values().stream());
+      case Collection<?> coll -> sumElements(coll.stream());
+      case Object[] arr -> sumElements(Arrays.stream(arr));
+      default -> 1;
     };
   }
 
-  private static int sumElements(Stream<?> stream, IdentityHashMap<Object, Boolean> visited) {
-    return stream.mapToInt(e -> count(e, visited)).sum();
+  private static int sumElements(Stream<?> stream) {
+    return stream.mapToInt(DataUtil::count).sum();
   }
 }
---
+++ java/sandbox-services/src/test/java/org/example/util/DataUtilTest.java
@@ -0,0 +1,27 @@
+package org.example.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class DataUtilTest {
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Test
+  void countAllKeys() throws IOException {
+    var jsonPath = Path.of("src/test/resources/data/complex-template-test.json");
+    var json = Files.readString(jsonPath, StandardCharsets.UTF_8);
+
+    Map<String, Object> data = objectMapper.readValue(json, Map.class);
+
+    var actual = DataUtil.countAllKeys(data);
+    var expected = 43;
+    assertEquals(expected, actual);
+  }
+}
